ffb982262a3aaf424cc57d82237571280a9f6dd8
Mahalingam <vmahalingam@deloitte.com>
2018-06-24 19:29:37 +0530

    12345

---
+++ server.js
@@ -0,0 +1,171 @@
+const _ = require('lodash');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {ObjectID} = require('mongodb');
+
+
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+var {Users} = require('./models/users');
+var {UserDetail} = require('./models/UserDetail');
+
+const port = process.env.PORT || 3000;
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/todos',(req,res)=>{
+
+  var todo = new Todo({
+    text : req.body.text
+  })
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.send(e);
+  })
+console.log(req.body);
+});
+
+app.get('/todos',(req,res)=>{
+
+  var todos1;
+  Todo.find().then((todos)=>{
+    res.send({
+      todos
+    })
+console.log(todos[0].havingCard);
+
+  },(e)=>{
+    res.status(400).send(e);
+  });
+
+
+
+
+
+});
+
+
+app.get('/todos123',(req,res)=>{
+
+  var todos1;
+  UserDetail.find().then((userdetails)=>{
+    res.send({
+      userdetails
+    })
+// console.log(todos[0].havingCard);
+
+  },(e)=>{
+    res.status(400).send(e);
+  });
+
+
+
+
+
+});
+
+
+
+app.get('/todos/:fromid&:toid',(req,res)=>{
+
+console.log("hii");
+var toid = req.params.toid;
+var fromid = req.params.fromid;
+
+  // var todo = new Todo({
+  //   having :  req.params.fromid
+  // });
+
+  // var todo1 = {
+  //
+  // having: req.params.fromid
+  // }
+
+  Todo.findOneAndUpdate({userCard:toid},{$push:{havingCard:fromid}},{new: true, upsert: true}
+  ).then((todo)=>{
+
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    return res.status(404).send();
+  });
+  });
+
+
+app.get('/users',(req,res)=>{
+
+  var todos1;
+  Users.find().then((users)=>{
+    res.send({
+      users
+    })
+
+
+
+  },(e)=>{
+    res.status(400).send(e);
+  });
+
+
+
+});
+
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+  return  res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{res.send(todo)},(e)=>{res.status(400).send();}).catch((e)=>{res.status(400).send();});
+
+});
+
+
+
+
+
+
+
+
+app.patch('/todos/:id',(req,res)=>{
+var id = req.params.id;
+var body = _.pick(req.body,['text','completed']);
+
+if(!ObjectID.isValid(id)){
+return  res.status(404).send();
+}
+if(_.isBoolean(body.completed)&&body.completed){
+  body.completedAt = new Date().getTime();
+}
+else{
+  body.completed = false;
+  body.completedAt = null
+}
+
+Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+  if(!todo){
+    return res.status(404).send();
+  }
+  res.send({todo});
+}).catch((e)=>{
+  return res.status(404).send();
+})
+});
+
+
+app.listen(port,()=>{
+  console.log(`started at port ${port}`);
+
+});
