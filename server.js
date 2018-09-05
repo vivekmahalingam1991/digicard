a709b7a3ed03467fe2cca20aef11cb1e5f587c37
vivekmahalingam1991 <39943016+vivekmahalingam1991@users.noreply.github.com>
2018-09-05 19:48:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var bodyParser = require('body-parser');
 
 var {ObjectID} = require('mongodb');
 
-
+var fs = require("fs");
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {Users} = require('./models/users');
 var {Userdetail} = require('./models/userdetail');
+var {Location} = require('./models/location');
 
 const port = process.env.PORT || 3000;
 
@@ -74,7 +75,45 @@ app.get('/todos123',(req,res)=>{
 
 });
 
+app.get('/display', function(req, res) {
+  fs.readFile('suntzu.jpg', function(err, data) {
+    if (err) throw err; // Fail if the file can't be read.
+    else {
+      res.writeHead(200, {'Content-Type': 'image/jpeg'});
+      res.end(data); // Send the file data to the browser.
+    }
+  });
+});
 
+app.get('/todos12345/:userid&:location',(req,res)=>{
+
+var location = req.params.location;
+var userid  = req.params.userid;
+
+Location.findOneAndUpdate({text:userid},{$set:{location123:location}},{new:true}).then((location)=>{
+console.log("hii");
+if(!location){
+  return res.status(404).send();
+}
+
+res.send({location});
+
+}).catch((e)=>{
+return res.status(404).send();
+})
+});
+
+app.get('/todos12345/:location',(req,res)=>{
+
+
+var location = req.params.location;
+
+Location.find({location123:location},{text:1}).then((location)=>{
+  res.send({location});
+
+});
+
+});
 
 app.get('/todos/:fromid&:toid',(req,res)=>{
 
