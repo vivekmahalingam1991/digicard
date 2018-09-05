c34850be06cd84c19ec15cf6593fa30835308ea6
vivekmahalingam1991 <39943016+vivekmahalingam1991@users.noreply.github.com>
2018-09-05 20:40:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -76,8 +76,8 @@ app.get('/todos123',(req,res)=>{
 });
 
 app.get('/display', function(req, res) {
-  res.send("hii");
-  fs.readFile('suntzu.jpg', function(err, data) {
+ 
+  fs.readFile('/suntzu.jpg', function(err, data) {
     if (err) throw err; // Fail if the file can't be read.
     else {
       res.writeHead(200, {'Content-Type': 'image/jpeg'});
@@ -92,7 +92,7 @@ var location = req.params.location;
 var userid  = req.params.userid;
 
 Location.findOneAndUpdate({text:userid},{$set:{location123:location}},{new:true}).then((location)=>{
-console.log("hii");
+
 if(!location){
   return res.status(404).send();
 }
